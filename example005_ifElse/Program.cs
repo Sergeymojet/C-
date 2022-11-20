7ccc8cd5b4c49fa712860a530203ca4be0a60cf2
Sergeymojet <sergeymojet@yandex.ru>
2022-11-20 15:20:07 +0300

    ex ifelse

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
